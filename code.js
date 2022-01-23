d8268785bf99bdc4c809f17e98295c4761b62907
root <mtrhoades0727@gmail.com>
2022-01-23 12:59:52 -1000

    first notes commit

---
+++ code.js
@@ -0,0 +1,36 @@
+console.log("Javascript Notes")
+
+/* Data Types 
+
+Strings: "Hello, World!" or 'Hello, World!'
+Booleans: True or False
+Numbers: 4, 2.3, 7.55
+Arrays: ["dog", "cat", "bird"] or [200, 623, -29]
+Objects: {name: "James", job: "software engineer"}
+         {length: 20, width: 30} // objects have key: value pairs
+
+
+/* if statements
+
+if (true) {
+    run code
+} */
+
+if (10 < 5) {
+    console.log("this code will not run because 10 is not less than 5")
+}
+if (10 > 5) {
+    console.log("this code will run because 10 is greater than 5")
+}
+
+/* else statements - (must go with if statements)
+
+else (false) {
+    run code
+} */
+
+if (10 < 5) {
+    console.log("this code will run because 10 is greater than 5")
+}else {
+    console.log("this code will not run becuase it is false.")
+}
