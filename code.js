f7949dd9d3e38f46a1bdc70cdd9df261839e83ca
root <mtrhoades0727@gmail.com>
2022-03-14 13:12:00 -1000

    updated codewars practice

--- code.js
+++ code.js
@@ -1656,6 +1656,31 @@ console.log(countSheep(
 
 
 
+// // Make a function that can take any non-negative integer as an argument and
+// return it with its digits in descending order.
+// Essentially, rearrange the digits to create the highest possible number.
+
+// EXAMPLES:
+// Input:     42145  --->  Output: 54421
+// Input:    145263  --->  Output: 654321
+// Input: 123456789  --->  Output: 987654321
+
+descendingOrder = (num) => {
+    return Number(num.toString().split('').sort((a, b) => b - a).join(''));    
+}
+    
+    // tests
+    console.log("-----Tests for Descending Order-----");
+    console.log("--If code is correct, console log shows true--");
+    console.log(descendingOrder(0) === 0);
+    console.log(descendingOrder(1) === 1);
+    console.log(descendingOrder(111) === 111);
+    console.log(descendingOrder(15) === 51);
+    console.log(descendingOrder(1021) === 2110);
+    console.log(descendingOrder(123456789) === 987654321);
+
+
+
 // Remove string spaces:
 noSpace = str => {
 
